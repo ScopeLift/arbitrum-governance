a178aa3397042d21e1a937c509d75dfbbf5a9887
garyghayrat <gary.ghayrat@pm.me>
2025-09-30 13:15:17 -0400

    Make fns virtual, mapping internal, and add natspec in GovernorV2

--- src/L2ArbitrumGovernorV2.sol
+++ src/L2ArbitrumGovernorV2.sol
@@ -8,34 +8,48 @@ import {
 } from "src/L2ArbitrumGovernor.sol";
 
 contract L2ArbitrumGovernorV2 is L2ArbitrumGovernor {
+    /// @notice Error thrown when attempting to cancel a proposal that is not in Pending state.
+    /// @param state The current state of the proposal.
     error ProposalNotPending(GovernorUpgradeable.ProposalState state);
+
+    /// @notice Error thrown when a non-proposer attempts to cancel a proposal.
+    /// @param sender The address attempting to cancel the proposal.
+    /// @param proposer The address of the actual proposer.
     error NotProposer(address sender, address proposer);
 
-    mapping(uint256 => address) private proposers;
+    /// @notice Mapping from proposal ID to the address of the proposer.
+    /// @dev Used in cancel() to ensure only the proposer can cancel the proposal.
+    mapping(uint256 => address) internal proposers;
 
+    /// @notice Creates a new proposal and records the proposer.
+    /// @param targets A list of target addresses for calls to be made in the proposal.
+    /// @param values A list of values (ETH) to be passed to the calls in the proposal.
+    /// @param calldatas A list of calldata for the calls in the proposal.
+    /// @param description The description for the proposal.
+    /// @return The id of the proposal.
     function propose(
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
         string memory description
-    ) public override(GovernorUpgradeable, IGovernorUpgradeable) returns (uint256) {
+    ) public virtual override(IGovernorUpgradeable, GovernorUpgradeable) returns (uint256) {
         uint256 _proposalId = GovernorUpgradeable.propose(targets, values, calldatas, description);
         proposers[_proposalId] = msg.sender;
         return _proposalId;
     }
 
     /// @notice Allows a proposer to cancel a proposal when it is pending.
-    /// @param targets A list of target addresses for calls to be made in the proposal.
-    /// @param values A list of values (ETH) to be passed to the calls in the proposal.
-    /// @param calldatas A list of calldata for the calls in the proposal.
-    /// @param descriptionHash The hash of the description for the proposal.
+    /// @param targets The proposal-to-be-cancelled's targets.
+    /// @param values The proposal's values.
+    /// @param calldatas The proposal's calldatas.
+    /// @param descriptionHash The hash of the proposal's description.
     /// @return The id of the proposal.
     function cancel(
         address[] memory targets,
         uint256[] memory values,
         bytes[] memory calldatas,
         bytes32 descriptionHash
-    ) public returns (uint256) {
+    ) public virtual returns (uint256) {
         uint256 _proposalId = hashProposal(targets, values, calldatas, descriptionHash);
 
         if (state(_proposalId) != ProposalState.Pending) {
